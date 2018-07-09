0f7a053ab592e6a1967b1f9a23f5c7618f0cbabc
A cang <wsp1034@163.com>
2018-07-09 15:16:11 +0800

    添加用户，删除用户完成

--- src/views/users/Users.vue
+++ src/views/users/Users.vue
@@ -12,7 +12,30 @@
         <el-input class="searchInput" clearable placeholder="请输入内容">
           <el-button slot="append" icon="el-icon-search"></el-button>
         </el-input>
-        <el-button type="success" plain>添加用户</el-button>
+        <el-button type="success" plain @click="handleUser=true">添加用户</el-button>
+          <el-dialog title="添加用户" :visible.sync="handleUser">
+            <el-form ref="formData"
+              :model="formData"
+              label-width="80px"
+              class="login-form">
+                <el-form-item label="用户名">
+                    <el-input v-model="formData.username"  placeholder="请输入用户名"></el-input>
+                </el-form-item>
+                <el-form-item label="密码">
+                    <el-input v-model="formData.password" type="password" placeholder="请输入密码"></el-input>
+                </el-form-item>
+                <el-form-item label="邮箱">
+                    <el-input v-model="formData.email"  placeholder="请输入邮箱"></el-input>
+                </el-form-item>
+                <el-form-item label="电话">
+                    <el-input v-model="formData.mobile"  placeholder="请输入电话"></el-input>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+              <el-button @click="handleUser = false">取 消</el-button>
+              <el-button type="primary" @click="handleUserAdd" >确 定</el-button>
+            </div>
+        </el-dialog>
       </el-col>
     </el-row>
     <!-- 表格区域 -->
@@ -70,11 +93,10 @@
         </template>
       </el-table-column>
        <el-table-column
-        prop=""
         label="操作">
         <template slot-scope="scope">
             <el-button type="primary" size="mini" plain icon="el-icon-edit"></el-button>
-             <el-button type="danger" size="mini" plain icon="el-icon-delete"></el-button>
+             <el-button type="danger" size="mini" plain icon="el-icon-delete" @click="handelDel(scope.row)"></el-button>
             <el-button type="success" size="mini" plain icon="el-icon-check" ></el-button>
         </template>
       </el-table-column>
@@ -87,7 +109,15 @@ export default {
   data () {
     return {
       list: [],
-      loading: true
+      loading: true,
+      handleUser: false,
+      formData: {
+        username: '',
+        password: '',
+        email: '',
+        mobile: '',
+        time: ''
+      }
     }
   },
   created () {
@@ -112,6 +142,52 @@ export default {
       } else {
         this.$message.error(msg)
       }
+    },
+    // 添加数据
+    async handleUserAdd () {
+      if (!this.formData.username || !this.formData.password) {
+        this.$message('用户名和密码不能为空')
+      } else {
+        // 发送请求之前，获取token
+        const token = sessionStorage.getItem('token')
+        // 在请求头中设置token
+        this.$http.defaults.headers.common['Authorization'] = token
+        this.formData.time = Number(new Date())
+        const formData = this.formData
+        const res = await this.$http.post('users', formData)
+        const data = res.data
+        const {meta: {status, msg}} = data
+        if (status === 201) {
+          this.$message.success('添加成功')
+          this.handleUser = false
+          this.locadata()
+        } else {
+          this.$message.error(msg)
+        }
+      }
+    },
+    // 删除数据
+    handelDel (row) {
+      this.$confirm('此操作将永久删除该用户, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => {
+          const id = row.id
+          this.$http.delete('users/' + id, function (res) {
+            const {meta: {status, msg}} = res.data
+            if (status === 200) {
+              this.$message({type: 'success', message: '删除成功!'})
+              this.locadata()
+            } else {
+              this.$message({type: 'error', message: msg})
+            }
+          })
+        })
+        .catch(() => {
+          this.$message({ type: 'info', message: '已取消删除' })
+        })
     }
   }
 }
