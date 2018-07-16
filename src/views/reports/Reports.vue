aebc9e117a2e6612e83d13a49fd1bb3f7b67f07a
A cang <wsp1034@163.com>
2018-07-16 16:19:46 +0800

    数据表

--- src/views/reports/Reports.vue
+++ src/views/reports/Reports.vue
@@ -2,13 +2,26 @@
   <el-card class="box-card">
     <!-- 面包屑 -->
      <my-breadcrumb level1="数据统计" level2="数据报表" class="mianbao"></my-breadcrumb>
-
   </el-card>
 </template>
 
 <script>
-export default {
 
+export default {
+  data() {
+    return {
+      option: {}
+    }
+  },
+  mounted() {
+
+  },
+  created() {
+
+  },
+  methods: {
+
+  }
 }
 </script>
 
